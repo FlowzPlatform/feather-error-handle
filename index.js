4958e184c0e51ff23cd71481d5814bcff7c3ff08
Sanjaysinh Zala <sanjaysinhz@officebrain.com>
2018-04-05 11:46:14 +0530

    First Version

--- index.js
+++ index.js
@@ -1,47 +1,48 @@
 const errors = require('feathers-errors');
+const jsonfile = require('jsonfile');
+let gErrMessages =  {
+  'INVALID_PORT': 'Port number should be in the range [1, 65535]',
+  'SERVICE_UNAVAIALBLE': 'Service not avaialble',
+  'REQLDRIVERERROR': 'RethinkDB service unavailable',
+  'PRAMATER_MISSING': 'Job data missing, Please provide body as paramater',
+  'CONTENT_TYPE': 'Content-Type should be application/json',
+  'INVALID_PRAMATER': 'Please provide valid paramater',
+  'NOTAUTHENTICATED': 'User authentication fail'
+}
 
-var gErrMessages = {
-    'ERR_INVALID_PORT': 'Port number should be in the range [1, 65535]',
-    'ERR_SERVICE_UNAVAIALBLE': 'Service not avaialble',
-    'ERR_REQLDRIVERERROR': 'RethinkDB service unavailable',
-    'ERR_PRAMATER_MISSING': 'Job data missing, Please provide body as paramater',
-    'ERR_CONTENT_TYPE': 'Content-Type should be application/json',
-    'ERR_INVALID_PRAMATER': 'Please provide valid paramater',
-    'ERR_NOTAUTHENTICATED': 'User authentication fail'
-}  
+if(process.env.errorMessages !== undefined) {
+  gErrMessages = require(process.env.errorMessages);
+}
 
-exports = module.exports = function errorHandler (options) {
+module.exports =  function errorHandler (options) {
   // get environment
-  var env = process.env.NODE_ENV || 'development'
+  var env = process.env.NODE_ENV || 'development';
 
   // get options
   var opts = options || {}
 
   // get log option
-  var log = opts.log === undefined
-    ? env !== 'test'
-    : opts.log
+  var log = opts.log === undefined ? env !== 'test' : opts.log;
 
   if (typeof log !== 'function' && typeof log !== 'boolean') {
-    throw new TypeError('option log must be function or boolean')
+    throw new TypeError('option log must be function or boolean');
   }
 
   // default logging using console.error
   if (log === true) {
-    log = logerror
+    log = logerror;
   }
-
   return function errorHandler (err, req, res, next) {
-    if (err.name == 'ReqlDriverError') {
-      throw new errors.GeneralError(gErrMessages.ERR_REQLDRIVERERROR);
-    } else if(err.name == 'NotAuthenticated') {
-      throw new errors.NotAuthenticated(gErrMessages.ERR_NOTAUTHENTICATED);
+    // let test
+    if(gErrMessages[err.name.toUpperCase()] !== undefined) {
+      throw new errors.GeneralError(gErrMessages[err.name.toUpperCase()]); 
     } else {
-      console.log('New Error From Feather-Error-Handler :: ', err);
+      console.log('New Error catch from FLOWZ-ERROR-HANDLER ::', err);
+      throw new Error(err)
     }
   }
 }
 
 function logerror (err, str) {
-  console.error(str || err.stack)
+  console.error(str || err.stack);
 }
