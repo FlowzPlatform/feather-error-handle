6753eedf78c38cf4404728180d0c0c93737f9321
Sanjaysinh Zala <sanjaysinhz@officebrain.com>
2018-04-25 11:49:40 +0530

    Badgatway and mongoDb error added and throw new error as it is

--- index.js
+++ index.js
@@ -7,22 +7,24 @@ let gErrMessages =  {
   'PRAMATER_MISSING': 'Job data missing, Please provide body as paramater',
   'CONTENT_TYPE': 'Content-Type should be application/json',
   'INVALID_PRAMATER': 'Please provide valid paramater',
-  'NOTAUTHENTICATED': 'User authentication fail'
+  'NOTAUTHENTICATED': 'User authentication fail',
+  'BADGATEWAY': 'API service not avaialble',
+  'MONGOERROR': 'MongoDB service unavailable'
 }
 
-if(process.env.errorMessages !== undefined) {
+if (process.env.errorMessages !== undefined) {
   gErrMessages = require(process.env.errorMessages);
 }
 
 module.exports =  function errorHandler (options) {
   // get environment
-  var env = process.env.NODE_ENV || 'development';
+  let env = process.env.NODE_ENV || 'development';
 
   // get options
-  var opts = options || {}
+  let opts = options || {}
 
   // get log option
-  var log = opts.log === undefined ? env !== 'test' : opts.log;
+  let log = opts.log === undefined ? env !== 'test' : opts.log;
 
   if (typeof log !== 'function' && typeof log !== 'boolean') {
     throw new TypeError('option log must be function or boolean');
@@ -34,11 +36,11 @@ module.exports =  function errorHandler (options) {
   }
   return function errorHandler (err, req, res, next) {
     // let test
-    if(gErrMessages[err.name.toUpperCase()] !== undefined) {
+    if (gErrMessages[err.name.toUpperCase()] !== undefined) {
       throw new errors.GeneralError(gErrMessages[err.name.toUpperCase()]); 
     } else {
       console.log('New Error catch from FLOWZ-ERROR-HANDLER ::', err);
-      throw new Error(err)
+      throw err
     }
   }
 }
